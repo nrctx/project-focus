3fcddfe1e21e17149eab83155f2aa6e5a6a67795
nrctx <marlon.g.viloria@gmail.com>
2026-03-13 20:48:32 -0500

    Add S3 backend and provider config

--- main.tf
+++ main.tf
@@ -7,6 +7,12 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket = "adhd-app-tfstate-535002852873"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
